ca01f62c4befc6b0b56a5b1ca14ffc1451ffe49f
Youssef Fray <yousseffray001@gmail.com>
2024-10-11 17:45:35 +0100

    add features to circuit breaker resilience4j

--- src/main/java/com/Inventory/Inventory_Management/CircuitBreakerController.java
+++ src/main/java/com/Inventory/Inventory_Management/CircuitBreakerController.java
@@ -1,12 +1,23 @@
 package com.Inventory.Inventory_Management;
 
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 public class CircuitBreakerController {
-    @GetMapping("/test-circuit-breaker")
+    private Logger logger = Logger.getLogger(String.valueOf(CircuitBreakerController.class));
+    @GetMapping("/circuit-breaker")
+    @Retry(name = "circuitBreaker", fallbackMethod = "hardCodedResponse")
     public String testCircuitBreaker() {
+        logger.info("Testing Circuit Breaker");
         return "Circuit Breaker is working";
     }
+public String hardCodedResponse(Exception e) {
+        return "Circuit Breaker is Open";
+    }
 }
+
+
